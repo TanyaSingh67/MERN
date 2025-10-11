56030a88ffe178e700e8d73d693c8a17d787c20e
TanyaSingh67 <tanyaplatformideas@gmail.com>
2025-10-11 16:35:24 +0530

    added module in json file and created hlowrld.js

---
+++ helowrld.js
@@ -0,0 +1,23 @@
+//node js is a runtime environment to run js code outside the browser
+//when v8 engine of chrome is combined with c++ it is called node js
+//its prdramming language is same as javascript
+//it only contains core java concepts and not dom or bom concepts
+//node js is used to build backend of web applications
+
+
+//we can print the output or se the results on the terminal of the vs code
+
+//we can run the js file on terminal using node command
+//node helowrld.js
+console.log("hello world");
+
+
+//we cannot run window alert or prompt in node js as they are part of bom and dom
+console.log(alert("hey"));
+console.log(prompt("hey"));
+
+//we can use npm to install packages in node js
+//npm is node package manager
+//npm comes preinstalled with node js
+//we can check the version of npm using npm -v command
+//we can check the version of node using node -v command
